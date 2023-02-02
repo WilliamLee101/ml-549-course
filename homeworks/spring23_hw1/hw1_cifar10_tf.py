cb6668dbee5a70068db0badc1f3ec905e36669fc
Yin-ching Lee <leewill@scc1.bu.edu>
2023-02-01 23:27:32 -0500

    upload my model for hw1

--- homeworks/spring23_hw1/hw1_cifar10_tf.py
+++ homeworks/spring23_hw1/hw1_cifar10_tf.py
@@ -21,8 +21,6 @@ from keras import layers
 import tensorflow_datasets as tfds
 from matplotlib import pyplot as plt
 
-
-
 if __name__ == '__main__':
 
     # Leave entity="bu-spark-ml" and project="hw1_spring2023"
@@ -30,7 +28,7 @@ if __name__ == '__main__':
     wandb.init(
         project="hw1_spring2023",  # Leave this as 'hw1_spring2023'
         entity="bu-spark-ml",  # Leave this
-        group="<your_BU_username>",  # <<<<<<< Put your BU username here
+        group="leewill",  # <<<<<<< Put your BU username here
         notes="Minimal model"  # <<<<<<< You can put a short note here
     )
 
@@ -56,9 +54,12 @@ if __name__ == '__main__':
     )
 
     # Optionally uncomment the next 3 lines to visualize random samples from each dataset
-    #fig_train = tfds.show_examples(ds_cifar10_train, ds_cifar10_info)
-    #fig_test = tfds.show_examples(ds_cifar10_test, ds_cifar10_info)
-    #plt.show()  # Display the plots
+    
+    print("""""""""""""""""""""""""""""""""""""")
+    fig_train = tfds.show_examples(ds_cifar10_train, ds_cifar10_info)
+    fig_test = tfds.show_examples(ds_cifar10_test, ds_cifar10_info)
+    plt.show()  # Display the plots
+    print("""""""""""""""""""""""""""""""""""""")
 
     def normalize_img(image, label):
         """Normalizes images: `uint8` -> `float32`."""
@@ -68,12 +69,12 @@ if __name__ == '__main__':
     ds_cifar10_train = ds_cifar10_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_cifar10_train = ds_cifar10_train.cache()     # Cache data
     ds_cifar10_train = ds_cifar10_train.shuffle(ds_cifar10_info.splits['train'].num_examples)
-    ds_cifar10_train = ds_cifar10_train.batch(32)  # <<<<< To change batch size, you have to change it here
+    ds_cifar10_train = ds_cifar10_train.batch(256)  # <<<<< To change batch size, you have to change it here
     ds_cifar10_train = ds_cifar10_train.prefetch(tf.data.AUTOTUNE)
 
     # Prepare cifar10 test dataset
     ds_cifar10_test = ds_cifar10_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    ds_cifar10_test = ds_cifar10_test.batch(32)    # <<<<< To change batch size, you have to change it here
+    ds_cifar10_test = ds_cifar10_test.batch(256)    # <<<<< To change batch size, you have to change it here
     ds_cifar10_test = ds_cifar10_test.cache()
     ds_cifar10_test = ds_cifar10_test.prefetch(tf.data.AUTOTUNE)
 
@@ -84,11 +85,47 @@ if __name__ == '__main__':
         # Edit code here -- Update the model definition
         # You will need a dense last layer with 10 output channels to classify the 10 classes
         # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        
+        #Add a 2d cnn layer here along with macpooling, batch normalization, and dropout layers
+        layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.BatchNormalization(momentum=0.03, axis=-1),
+        layers.Dropout(0.2),
+        
+        #repeat the first few layers four more times
+        layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.BatchNormalization(momentum=0.03, axis=-1),
+        layers.Dropout(0.2),
+        
+        layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.BatchNormalization(momentum=0.03, axis=-1),
+        layers.Dropout(0.2),
+        
+        layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.BatchNormalization(momentum=0.03, axis=-1),
+        layers.Dropout(0.2),
+        
+        layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.BatchNormalization(momentum=0.03, axis=-1),
+        layers.Dropout(0.2),
+        
+
         layers.Flatten(),
-        layers.Dense(128, activation='relu'),
+        #Add an additional linear layer here, the decision was made from experimentation
+        layers.Dense(256, activation='relu', kernel_regularizer='l2'),
+        # layers.Dense(128, activation='relu', kernel_regularizer='l2'),
+        # layers.Dense(128, activation='relu', kernel_regularizer='l2'),
+        # layers.Dense(64, activation='relu', kernel_regularizer='l2'),
+        # layers.Dense(32, activation='relu', kernel_regularizer='l2'),
+        layers.Flatten(),
         # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         tf.keras.layers.Dense(10)
     ])
+    
 
     # Log the training hyper-parameters for WandB
     # If you change these in model.compile() or model.fit(), be sure to update them here.
@@ -96,22 +133,23 @@ if __name__ == '__main__':
         #####################################
         # Edit these as desired
         # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        "learning_rate": 0.001,
+        #These parameters are decided based on experimentations
+        "learning_rate": 0.0005,
         "optimizer": "adam",
-        "epochs": 5,
-        "batch_size": 32
+        "epochs": 30,
+        "batch_size": 256
         # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     }
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=.001),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=.0005),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
     )
 
     history = model.fit(
         ds_cifar10_train,
-        epochs=5,
+        epochs=30,
         validation_data=ds_cifar10_test,
         callbacks=[WandbMetricsLogger()]
     )
